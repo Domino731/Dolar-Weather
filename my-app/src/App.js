d26c217d8e1869c0bd0ffe07d2a2849d53a3c662
Domino731 <d.orzechowski18@gmail.com>
2021-05-31 10:57:44 +0200

    nav

---
+++ my-app/src/App.js
@@ -0,0 +1,11 @@
+import "./sass/main.scss"
+import {Navigation} from "./components/Navigation";
+function App() {
+  return (
+    <>
+     <Navigation/>
+    </>
+  );
+}
+
+export default App;
---
+++ my-app/src/components/Navigation.js
@@ -0,0 +1,14 @@
+export const Navigation = () => {
+    return (
+        <nav className="container">
+            <div className="navigation">
+                
+                <div className="navigation__menuIcon">
+                    <i className="fas fa-bars"/>
+                </div>
+
+                <input type="text" className="navigation__searchBar"/>
+            </div>
+        </nav>
+    )
+}
