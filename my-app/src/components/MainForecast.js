f9f1acc8535e532791c58cdaf0d5ef1a4b3344d4
Domino731 <d.orzechowski18@gmail.com>
2021-05-31 19:49:58 +0200

    api

--- my-app/src/components/MainForecast.js
+++ my-app/src/components/MainForecast.js
@@ -1,11 +1,22 @@
 import {connect} from "react-redux";
-import {useState, useEffect} from "react";
+import {useEffect} from "react";
 
 const MainForecast = ({city}) => {
-    const [forecast, setForecast] = useState(" ");
+
+    useEffect(() => {
+        fetch(`http://api.openweathermap.org/data/2.5/weather?q=london&appid=e105d21fb59c10d6ab6a04386a749917`)
+            .then(r => r.text())
+            .then(r => console.log(r))
+    }, [])
+
 
     return (
-        <h1>{city.location}</h1>
+
+
+        <div>
+            <h1>{city.location}</h1>
+            <button>asdasdasdasdasd</button>
+        </div>
     )
 }
 
---
+++ my-app/src/functions/getWeather.js
@@ -0,0 +1,2 @@
+const key = `e105d21fb59c10d6ab6a04386a749917`;
+
